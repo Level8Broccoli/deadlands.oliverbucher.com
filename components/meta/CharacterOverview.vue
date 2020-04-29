bcad4c07028b4057a86241c9d81f4423779969dc
Oliver Bucher <level8broccoli@gmail.com>
2020-04-29 16:57:18 +0200

    overview dynamic

--- components/meta/CharacterOverview.vue
+++ components/meta/CharacterOverview.vue
@@ -23,13 +23,13 @@
         <div class="control">
           <div class="tags has-addons">
             <span class="tag is-dark">Parade/Zielwert (Kämpfen)</span>
-            <span class="tag">2</span>
+            <span class="tag">{{ 2 + fight / 2 }}</span>
           </div>
         </div>
         <div class="control">
           <div class="tags has-addons">
             <span class="tag is-dark">Robustheit (Konstitution)</span>
-            <span class="tag">6 + Panzerung</span>
+            <span class="tag">{{ 2 + vigor / 2 }} + Panzerung</span>
           </div>
         </div>
         <div class="control">
@@ -50,6 +50,20 @@
 
 <script>
 export default {
-  name: 'CharacterOverview'
+  name: 'CharacterOverview',
+  computed: {
+    fight() {
+      const savedEntry = this.$store.state.charSave.skillList.find(
+        (e) => e.id === 'Kaempfen'
+      )
+      return savedEntry ? savedEntry.value : 0
+    },
+    vigor() {
+      const savedEntry = this.$store.state.charSave.attributeList.find(
+        (e) => e.id === 'Konstitution'
+      )
+      return savedEntry ? savedEntry.value : 4
+    }
+  }
 }
 </script>
