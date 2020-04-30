1c56a6ea824d52cfdd9c013f3a002791ed92beaa
Oliver Bucher <level8broccoli@gmail.com>
2020-04-30 12:30:14 +0200

    reset component

--- components/chronicle/ChronicleEntry.vue
+++ components/chronicle/ChronicleEntry.vue
@@ -1,94 +1,19 @@
 <template>
   <tbody>
     <tr>
-      <td>
-        <GameButton
-          button-type="roll"
-          title="Wurf wiederholen"
-          @button-click="rollDice(entry)"
-        />
-      </td>
-      <td>
-        <!-- {{ entry.comment }}
-        <div class="has-text-weight-bold">
-          <small>{{ entry.timestamp }}</small>
-        </div> -->
-      </td>
-      <td>
-        <!-- <div class="buttons">
-          <button
-            v-for="die in entry.rollNormal"
-            :key="die"
-            class="button is-light"
-            :class="renderDice(die.rolled, die.dice)"
-          >
-            {{ die.rolled }}
-          </button>
-        </div> -->
-      </td>
-      <td>
-        <!-- <div class="buttons">
-          <button
-            v-for="die in entry.rollWild"
-            :key="die"
-            class="button is-light"
-            :class="renderDice(die.rolled, die.dice)"
-          >
-            {{ die.rolled }}
-          </button>
-        </div> -->
-      </td>
-      <td>
-        <!-- <div class="buttons">
-          <button
-            v-for="mod in entry.modifications"
-            :key="mod"
-            class="button is-light"
-            :class="mod.value < 0 ? 'is-danger' : 'is-success'"
-          >
-            {{ mod.name }} {{ mod.value }}
-          </button>
-        </div> -->
-      </td>
-      <td>
-        <!-- <button class="button" :class="renderResult(entry.result)">
-          {{ entry.result.value }}
-        </button> -->
-      </td>
-      <td>
-        <!-- <div v-if="entry.result.success" class="has-text-success">Erfolg!</div>
-        <div v-else-if="entry.result.critFailure" class="has-text-danger">
-          Snake Eyes!
-        </div>
-        <div v-else class="has-text-warning">Fehlschlag</div>
-        <div v-if="entry.result.raise === 1" class="has-text-info">
-          1 Steigerung
-        </div>
-        <div v-if="entry.result.raise > 1" class="has-text-info">
-          {{ entry.result.raise }} Steigerungen
-        </div> -->
-      </td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
     </tr>
   </tbody>
 </template>
 
 <script>
-import GameButton from '~/components/common/GameButton'
 export default {
-  name: 'ChronicleEntry',
-  components: { GameButton },
-  props: {
-    entry: {
-      type: Object,
-      default() {
-        return {}
-      }
-    }
-  },
-  methods: {
-    rollDice(entry) {
-      // TODO
-    }
-  }
+  name: 'ChronicleEntry'
 }
 </script>
