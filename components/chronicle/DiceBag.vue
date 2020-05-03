7cba83cd7fe227807e1f1842dd0b71420f37b3b5
Oliver Bucher <level8broccoli@gmail.com>
2020-05-03 08:19:47 +0200

    dicebag update

--- components/chronicle/DiceBag.vue
+++ components/chronicle/DiceBag.vue
@@ -1,19 +1,24 @@
 <template>
-  <nav class="level is-mobile">
-    <div v-for="die in dice" :key="die" class="level-item has-text-centered">
-      <div>
-        <p class="title">
-          <GameButton
-            :title="'Wirf einen ' + die + ' Würfel'"
-            button-type="text"
-            :button-text="'W ' + die"
-            :button-styles="['info', 'light']"
-            @button-click="rollDice(die)"
-          />
-        </p>
+  <div class="box">
+    <nav class="level">
+      <div class="level-item has-text-centered">
+        <div><p class="title has-text-weight-bold">Würfel</p></div>
       </div>
-    </div>
-  </nav>
+      <div v-for="die in dice" :key="die" class="level-item has-text-centered">
+        <div>
+          <p class="title">
+            <GameButton
+              :title="'Wirf einen ' + die + ' Würfel'"
+              button-type="text"
+              :button-text="'W ' + die"
+              :button-styles="['info', 'light']"
+              @button-click="rollDice(die)"
+            />
+          </p>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
