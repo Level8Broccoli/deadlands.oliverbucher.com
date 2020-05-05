34f265f79a798ed9febde0d9fd0cace312d27e86
Oliver Bucher <level8broccoli@gmail.com>
2020-05-05 19:55:44 +0200

    remove console.logs

--- plugins/websocketPlugin.js
+++ plugins/websocketPlugin.js
@@ -6,21 +6,23 @@ export default function websocketPlugin({ store }) {
   let sendMessage = (msg) => {}
 
   const socket = new SockJS(WS_ENDPOINT)
+
   socket.onopen = function() {
     store.commit('chronicle/setWsConnection', true)
-    console.log('[WS] Connection opened')
     sendMessage = (msg) => socket.send(msg)
   }
+
   socket.onmessage = function(e) {
     const parse = JSON.parse(e.data)
     store.dispatch('chronicle/commitOtherAction', parse)
     store.dispatch('charSave/updateCharNames', parse)
     store.dispatch('udpateCardDeck', parse)
   }
+
   socket.onclose = function() {
     store.commit('chronicle/setWsConnection', false)
-    console.log('[WS] Connection closed')
   }
+
   store.subscribe((mutation, state) => {
     if (
       mutation.type === 'chronicle/addToChronicle' &&
