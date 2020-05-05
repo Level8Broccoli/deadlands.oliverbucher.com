c9c319a212f6f83f4b83365bbfe7dea4e581a209
Oliver Bucher <level8broccoli@gmail.com>
2020-05-05 21:25:14 +0200

    automatic reconnect WS

--- plugins/websocketPlugin.js
+++ plugins/websocketPlugin.js
@@ -5,23 +5,27 @@ const WS_ENDPOINT = 'https://deadlands-echo.herokuapp.com/echo'
 export default function websocketPlugin({ store }) {
   let sendMessage = (msg) => {}
 
-  const socket = new SockJS(WS_ENDPOINT)
+  const connect = () => {
+    const socket = new SockJS(WS_ENDPOINT)
 
-  socket.onopen = function() {
-    store.commit('chronicle/setWsConnection', true)
-    sendMessage = (msg) => socket.send(msg)
-  }
+    socket.onopen = function() {
+      store.commit('chronicle/setWsConnection', true)
+      sendMessage = (msg) => socket.send(msg)
+    }
 
-  socket.onmessage = function(e) {
-    const parse = JSON.parse(e.data)
-    store.dispatch('chronicle/commitOtherAction', parse)
-    store.dispatch('charSave/updateCharNames', parse)
-    store.dispatch('udpateCardDeck', parse)
-  }
+    socket.onmessage = function(e) {
+      const parse = JSON.parse(e.data)
+      store.dispatch('chronicle/commitOtherAction', parse)
+      store.dispatch('charSave/updateCharNames', parse)
+      store.dispatch('udpateCardDeck', parse)
+    }
 
-  socket.onclose = function() {
-    store.commit('chronicle/setWsConnection', false)
+    socket.onclose = function() {
+      store.commit('chronicle/setWsConnection', false)
+      setTimeout(connect, Math.random() * 5000)
+    }
   }
+  connect()
 
   store.subscribe((mutation, state) => {
     if (
